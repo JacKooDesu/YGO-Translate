0725e805230deb92fe90b348aed7492d42807ef9
JackooDesu <45838705+JacKooDesu@users.noreply.github.com>
2022-11-12 13:22:11 +0800

    no message

--- Asset Uncompressor/Program.cs
+++ Asset Uncompressor/Program.cs
@@ -1,28 +1,27 @@
 ﻿using Ionic.Zlib;
-using Ionic.Zip;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //byte[] data = File.ReadAllBytes(".\\files\\test");
-        //byte[] ch = new byte[data.Length];
-        //byte[] v1 = new byte[data.Length];
-        //int iter = 0;
-        //foreach(byte b in data)
-        //{
-        //    ch[iter] = (byte)(b ^ (0xB3 * ((iter + 0x2f0) % 0x800000FF)));
-        //    v1[iter] = (byte)(ch[iter] ^ (iter % 0x7));
-        //    iter++;
-        //}
+        byte[] data = File.ReadAllBytes(".\\files\\test");
+        byte[] ch = new byte[data.Length];
+        byte[] v1 = new byte[data.Length];
+        int iter = 0;
+        foreach (byte b in data)
+        {
+            ch[iter] = (byte)(b ^ (0xB3 * ((iter + 0x2f0) % 0x800000FF)));
+            v1[iter] = (byte)(ch[iter] ^ (iter % 0x7));
+            iter++;
+        }
 
 
-        //File.WriteAllBytes(".\\files\\output.txt", ZlibStream.UncompressBuffer(v1));
+        File.WriteAllBytes(".\\files\\output.txt", ZlibStream.UncompressBuffer(v1));
 
-        byte[] data = File.ReadAllBytes(".\\files\\modify");
-        Console.WriteLine(data.Length);
-        var after = CompressBuffer(data);
-        File.WriteAllBytes(".\\files\\compressed", after);
+        //byte[] data = File.ReadAllBytes(".\\files\\modify");
+        //Console.WriteLine(data.Length);
+        //var after = CompressBuffer(data);
+        //File.WriteAllBytes(".\\files\\compressed", after);
     }
 
     static byte[] CompressBuffer(byte[] b)
